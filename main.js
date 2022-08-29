e691b4e0a0066fa394d285f82ea79a41f140690b
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-28 23:42:55 -0500

    First commit, challenge Arjuna 29/08/2022

---
+++ main.js
@@ -0,0 +1,65 @@
+/* EJERCICIO #1 -- 
+Programa una función que realice la suma de 3 parámetros, los valores serán pasados por argumentos. */
+
+function sumar(num1, num2, num3) {
+  // Tu código aquí 👈 
+};
+
+
+
+
+
+
+/* EJERCICIO #2 -- 
+Pide al usuario la edad y si es mayor de 18 años indica que ya puede entrar a la discoteca, si tiene menos de 18, entonces es menor de edad y no puede ingresar */
+
+// Tu código aquí 👇​ 
+
+
+
+
+
+
+/* EJERCICIO #3 -- 
+Programa una función que cuente el número de caracteres de una cadena de texto que envías por parámetro, sino no envías nada debe salir un mensaje de error (Debes ingresar una palabra o frase)
+ Ejemplo: si miFunción("Hola Mundo"), entonces devolverá 10 caracteres */
+
+function contarCaracteres() {
+  // Tu código aquí 👈 
+}; 
+
+
+
+
+
+
+/* EJERCICIO #4 --
+Tienes un Array de animales, debes agregar un elemento 'TIBURON' al final del Array, un elemento 'MARIPOSA' al inicio del Array, elimina el último elemento, elimina el primer elemento. Ir mostrando el resultado a medida que ejecutas una acción. */
+
+const animales = ['GATO', 'GALLINA', 'PERRO', 'ELEFANTE', 'CONEJO']
+// Tu código aquí 👇​ 
+
+
+
+
+
+
+/* EJERCICIO #5 -- 
+Combina dos arrays de números en uno solo
+En este desafío recibirás dos arrays de números como parámetros de entrada y debes retornar un array que tenga los dos arrays unidos.
+Pista: en este ejercicio puedes Googlear, que te puede servir para tener dos Arrays en uno */
+
+function solucion(arrayA, arrayB) {
+  // Tu código aquí 👈 
+}; 
+
+
+
+
+
+
+/* EJERCICIO #6 -- 
+Crea un objeto Persona, y agrégale las siguientes propiedades: (nombre --> Giovani), (apellido --> Bautista), (edad --> 40), (ubicacion --> Funza), (un Array de hobbies --> manejar moto, cocinar, bailar) (un objeto de amigos --> Arjuna, Jairo, Sebastián) 
+mostrar una por una de las propiedades en una lista */
+
+// Tu código aquí 👇​ 
